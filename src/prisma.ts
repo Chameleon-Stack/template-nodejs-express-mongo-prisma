60b35c0f884fc7421f1b7770c5eef0651fe9ff1c
ibsiany <ibsiany.dias@seidor.com>
2024-07-04 14:16:07 -0300

    chore: correcting

--- src/prisma.ts
+++ src/prisma.ts
@@ -2,3 +2,7 @@ import 'reflect-metadata';
 import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
+
+// export const prisma = new PrismaClient({
+//   log: ['query', 'info', 'warn', 'error'],
+// });
